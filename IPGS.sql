a72ce83defce9aa082f81ad69fed45d7e1abadc3
unknown <andiosdroid@gmail.com>
2016-01-04 22:53:10 +0530

    Added Destructor and initialized IPGSdb

--- IPGS.sql
+++ IPGS.sql
@@ -1,11 +1,8 @@
 /* 
 Doubts as of now:
-1) Should I use varchar() instead of text
-2)In Comments table I want to make C_id & C_SqNO as primary key. dont know how to make it.
-3)Using Issue type as an Enum. Is it alright ? or should I use array or some other data type 
-as I wish to restrain the choices to limited choices
-4) not sure if the database overall uses the right design schema. Didn't make it totally normalized.(Not so sure :P)
 
+1) not sure if the database overall uses the right design schema. Didn't make it totally normalized.(Not so sure :P)
+2)Implemeting Blobs in image storage(Issues Table)
 */
 
 
