06d3aa2f214b8e9845876c22e1efd14b5bdfb455
Devyash Sanghai <andiosdroid@gmail.com>
2016-01-23 19:09:29 +0530

    Compiling file

--- IPGSdb.py
+++ IPGSdb.py
@@ -4,58 +4,79 @@
 #
 import psycopg2
 
+#Kind of understanding closures and decorators but what the heck!
 def getConnection(f):
-	def getConnectionInner(*args, **kwargs):
-		global conn
-		if not conn:
-			conn = psycopg2.connect("dbname = IPGS")
-		try:
-    		rv = f(conn, *args, **kwargs)
-    	except Exception, e:
-    		cnn.rollback() 
-    		raise
-		else:
-    		cnn.commit() # or maybe not
-		finally:
-        	return rv
-		return getConnectionInner
-		#return a connection object
-	#return con 
-	return conn
+    def getConnectionInner(*args, **kwargs):
+        # or use a pool, or a factory function...
+        conn = psycopg.connect("dbname = IPGS")
+        try:
+            rv = f(conn, *args, **kwargs)
+        except Exception, e:
+            conn.rollback()
+            raise
+        else:
+            conn.commit() # or maybe not
+        finally:
+            conn.close()
+
+        return rv
+
+    return getConnectionInner
+
 	
 def getNearbyIssues(lat, lng):
 	#within a given area lat (lat +1 mintue - 1 mintue as 1 minute is 1.8 kms ) 
 	#Select I_ID from Comments where (I_lat>lat-1 min & I_lat<lat+1 min)&&(I_lng>lng-1 min & I_lng<lng+1 min)
 	#return array OF Issue ID
 
-def getAllIssues(array of I_Id):
+	pass
+
+
+@getConnection
+def getAllIssues(I_Ids):
+	#I_Ids is a array of issue I_Id
 	#Call  if (isI_Visible(I_Id))=> true then proceed ahead else dont return that issue  
 	#return: a dictionary that will contain image,I_title,IssueID,votes(will call getvotes for this to return),I_lat,I_lng,I_type WITHIN LIMIT 20(So the webpage doesn't hang
-	#A_Issue object
+	#A_Issue dictionary
+
+	c=conn.cursor() 
+	for i in I_Ids:
+		if(isI_Visible(i)):
+			c.execute("""SELECT I_Id, I_Author, I_Title, I_Content, I_Lat, I_Lng, I_Image, I_AnonFlag, I_Type, I_time FROM Issues WHERE I_Id= %s),(i,);""")
+			A_Issues[i] = ({'I_Id': str(row[0]), 'I_Author': str(row[1]),'I_Title': str(row[2]),'I_Content': str(row[3]),'I_Lat': str(row[4]),'I_Lng': str(row[5]),'I_AnonFlag': str(row[6]),'I_Type': str(row[7]),'I_time': str(row[8])}, c.fetchone())
+		pass
+	return A_Issues
+
 
 def getIssues(I_Id): 
 	#Displaying Everything we have on that issue
+	#Check if the I_AnonFlag is set, if it is set then dont return aut hor of the issue
 	#return lat, lng, title, content, image, Author, I_type, timestamp, Votes
+	pass
 
 def getComment(I_Id):
 	#Display all the comments presented by the user.
 	#return
+	pass
 
 def setNewIssue(lat,lng, title, content,image, anonflag,Author,I_type,timestamp):
 	# store the data in the data base
 	#INSERT
+	pass
 
 def getMyIssues(U_Id):
 	#this fuction will return a list of I_Id which have author as U_Id passed
-	#return dictionary 
+	#return dictionary
+	pass 
 
 def setComments(I_Id, U_Id, C_Content, C_time):
 	# insert into Comments values(U_Id, C_Content,  , I_ID)
-	#
+	pass
 
 def setVote(U_Id,I_Id):
 	#if U_Id not present in votes for I_Id then vote
 	#Insert into votes
+	pass
 
 @getConnection
 def getVote(I_Id):
@@ -68,26 +89,26 @@ def getVote(I_Id):
 	return likes, dislikes
 	#return number of flags set for a I_ID
 	#count(*) from votes where I_Id=I_Id from Issues group by V_flag
+	pass
 
 def getAboutus():
 	#Static page about the us
-	#
+	pass
 
-#Kind of understanding closures and decorators but what the heck!
-
-    
 @getConnection
 def getUserSatisfaction(I_Id):
 	#calls getvotes() and if likes > dislike returns likes/total votes
-	#
+	pass
 
 def getMap():
 	#will load raw get Map in the form of java script
 	#this will be in a div tag within a box
+	pass
 
 def setMarkers(A_Issue):
 	#this will set markers on the map based on I_type
 	#this will call getAllIssues and get the basic detials of all the issues and set the marker accordingly
+	pass
 
 @getConnection
 def setI_Visible(I_Id, U_Id, I_Author,I_Visible):
@@ -96,16 +117,16 @@ def setI_Visible(I_Id, U_Id, I_Author,I_Visible):
 	#Returns true if the change was successful else returns false
 	c=conn.cursor()
 	try:
-		if U_Id = I_Author :
-		c.execute("""UPDATE Issues SET I_Visible= %s WHERE I_Id=%s""",(I_Visible,I_Id,))
-		c.close()
-		return true
+		if U_Id == I_Author :
+			c.execute("""UPDATE Issues SET I_Visible=%s WHERE I_Id=%s""",(I_Visible,I_Id,))
+			c.close()
+			return true
 		else:
 			print "User ID and Author ID donot match. You do not have access to suspend this Issue."
 	except Exception, e:
 		print "Exception im setting the Issue Visibility"
 	else:
-	return false
+		return false
 
 @getConnection
 def isI_Visible(I_Id):
@@ -118,6 +139,7 @@ def isI_Visible(I_Id):
 		return value
 	except Exception, e:
 		print "Exception in isI_Visible function "
+	pass
 	
 @getConnection
 def deleteComment(C_Id, C_SqNo):
@@ -129,7 +151,8 @@ def deleteComment(C_Id, C_SqNo):
 	except Exception, e:
 		print "Exception in delete comment"
 	else:
-	return false
+		return false
+	pass
 
 	#This function will return true if the comment is deleted successfully else false
 
@@ -145,7 +168,8 @@ def deleteIssue(I_Id):
 	except Exception, e:
 		print "Exception in delete Issues"
 	else:
-	return false
+		return false
+	pass
 
 @getConnection
 def deleteVote(V_IssueId, V_Author):
@@ -159,5 +183,6 @@ def deleteVote(V_IssueId, V_Author):
 	except Exception, e:
 		print "Exception in delete Vote"
 	else:
-	return false
+	 return false
+	pass
 
