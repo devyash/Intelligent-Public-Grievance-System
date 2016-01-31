a5b1445044c2c313ae3ed862600b5a1c67b69748
Devyash Sanghai <andiosdroid@gmail.com>
2016-01-31 14:27:24 +0530

    update trial.sql

--- trial.sql
+++ trial.sql
@@ -1,3 +1,7 @@
 SELECT I_Id, I_Author, I_Title, I_Content, I_Lat,
 				I_Lng, I_Image, I_AnonFlag, I_Type, I_time, I_Image
 				 FROM Issues WHERE I_Id = 220;
+IF( NOT EXITS(SELECT * FROM Votes WHERE V_IssueId=%s AND V_Author=%s))
+					 BEGIN 
+					 INSERT INTO Comments (I_Id,U_Id,V_Flag)
+					 END
