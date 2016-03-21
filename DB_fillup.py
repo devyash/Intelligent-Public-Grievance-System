407df27f2a8a77986b18d4d4c5490befc968c2f3
Devyash Sanghai <andiosdroid@gmail.com>
2016-03-21 17:45:59 +0530

    Made the db of project to work

---
+++ DB_fillup.py
@@ -0,0 +1,66 @@
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from Db_setup import Base, Issue, Vote, Comment, User
+# Connect to Database and create database session
+engine = create_engine('sqlite:///IPGS.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+user1=User(id=1, email="devyashsanghai@gmail.com", name="Devyash")
+session.add(user1)
+session.commit()
+
+user2=User(email="abada@gmail.com", name="Abada")
+session.add(user2)
+session.commit()
+
+user3=User(email="ka@gmail.com", name="Ka")
+session.add(user3)
+session.commit()
+
+issue1=Issue(user=user1,title="Big Issue 1",
+	content="My problem is BLAH BLAH BLAH BLAH......................................",lat=19.3,
+	lng=72.86,
+	image="C://images//abx.jpg",
+	anonFlag=0, type=1)
+session.add(issue1)
+session.commit()
+
+issue2=Issue(user=user1,title="Big Issue 2",
+	content="HAHAHAHAHAHHAHAHHAHAHAHAAHAHAH",
+	lat=20.0,
+	lng=72.86,
+	image="C://images//abx.jpg",
+	anonFlag=0, type=1)
+session.add(issue2)
+session.commit()
+
+issue3=Issue(user=user1,title="Big Issue 3",
+	content="ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	lat=19.3,
+	lng=72.86,
+	image="C://images//abx.jpg",
+	anonFlag=0, type=1)
+session.add(issue3)
+session.commit()
+
+comment1=Comment(issue=issue3, user=user1, content="Boring Comment 1")
+session.add(comment1)
+session.commit()
+
+comment2=Comment(issue=issue3, user=user2, content="Boring Comment 2")
+session.add(comment2)
+session.commit()
+
+comment3=Comment(issue=issue3, user=user1, content="Boring Comment 3")
+session.add(comment3)
+session.commit()
+
+
+vote1=Vote( issue=issue1, user=user1, like=1, dislike=0)
+session.add(vote1)
+session.commit()
+
+print "added DB items!"
---
+++ DB_setup.py
@@ -0,0 +1,61 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric, Boolean, UniqueConstraint, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+
+class User(Base):
+	__tablename__='user'
+	id=Column(Integer,primary_key=True)
+	email=Column(String(80),unique=True)
+	name=Column(String(80),nullable=False)
+	#gender=Column(String(8),nullable=False)
+	#strAdr=Column(String(255))
+	#city=Column(String(10),nullable=False)
+	#pincode=Column(Integer,nullable=False)
+	#dob=Column(Date)
+	#admin=Column(Boolean,default=False)
+
+class Issue(Base):
+	__tablename__='issue'
+	#On delete left, timezone left, also relztionship pending,current Timestamp pending too,Date time pending too
+	id=Column(Integer,primary_key=True)
+	author=Column(Integer,ForeignKey('user.id',ondelete="CASCADE"))
+	user=relationship(User)
+	title=Column(String(80),nullable=False)
+	content=Column(String(255),nullable=False)
+	lat=Column(Numeric,nullable=False)
+	lng=Column(Numeric,nullable=False)
+	image=Column(String(250))
+	anonFlag=Column(Boolean,default=False)
+	type=Column(Integer,nullable=False)
+	#time=Column(Date)
+	#visible=Column(Boolean,default=True)
+
+class Comment(Base):
+	__tablename__='comment'
+	#pending time
+	id=Column(Integer,ForeignKey('issue.id',ondelete="CASCADE"))
+	issue=relationship(Issue)
+	author=Column(Integer,ForeignKey('user.id',ondelete="CASCADE"))
+	user=relationship(User)
+	content=Column(String(250))
+	#time=Column(Date)
+	sqNo=Column(Integer,primary_key=True)
+
+class Vote(Base):
+	__tablename__='vote'
+	#pending: time 
+	id=Column(Integer,ForeignKey('issue.id',ondelete="CASCADE"),primary_key=True)
+	issue=relationship(Issue)
+	author=Column(Integer,ForeignKey('user.id',ondelete="CASCADE"),primary_key=True)
+	user=relationship(User)
+	like=Column(Integer,default=0)
+	dislike=Column(Integer,default=0)
+	#time=Column(Date)	
+
+engine=create_engine('sqlite:///IPGS.db')
+Base.metadata.create_all(engine)
